7a260705588686b97ec02904dc6bbd1da8279f8a
oq-Yuki-op <o.yukinko0715@gmail.com>
2021-09-13 17:04:59 +0900

    Add Models and db setting, logger

--- app/models/setting.py
+++ app/models/setting.py
@@ -1,9 +1,14 @@
 import os
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, create_engine
+from sqlalchemy import Column, DateTime, MetaData, create_engine
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.declarative import declarative_base, declared_attr
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy_utils import create_database, database_exists
+
+from app import app_logger
+from app.responses.error import DataBaseConnectionError, InternalServerError
 
 # Engine
 SERVER = os.getenv('POSTGRES_SERVER')
@@ -19,14 +24,27 @@ Engine = create_engine(
 )
 
 # Session
-session = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=Engine
+session = scoped_session(
+    sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=Engine
+    )
 )
 
 
 class Base(object):
+    """BaseModel Class
+
+    Attributes
+    -------
+    created_at : Column
+
+    updated_at : Column
+    """
+
+    __table_args__ = {'schema': os.environ.get('DB_SCHEMA', None)}
+
     @declared_attr
     def created_at(cls):
         return Column(DateTime, default=datetime.now, nullable=False)
@@ -36,4 +54,35 @@ class Base(object):
         return Column(DateTime, default=datetime.now, nullable=False)
 
 
-BaseModel = declarative_base(cls=Base)
+metadata = MetaData(naming_convention={
+    'pk': 'pk_%(table_name)s',
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+    'ix': 'ix_%(table_name)s_%(column_0_name)s',
+    'uq': 'uq_%(table_name)s_%(column_0_name)s',
+    'ck': 'ck_%(table_name)s_%(constraint_name)s',
+})
+
+BaseModel = declarative_base(cls=Base, metadata=metadata)
+
+
+def initialize_db() -> None:
+    try:
+        if not database_exists(Engine.url):
+            app_logger.info('Create Database')
+            create_database(Engine.url)
+    except SQLAlchemyError as e:
+        app_logger.error(e)
+        raise DataBaseConnectionError()
+    except Exception as e:
+        app_logger.error(e)
+        raise InternalServerError()
+
+def initialize_table() -> None:
+    try:
+        BaseModel.metadata.create_all(Engine)
+    except SQLAlchemyError as e:
+        app_logger.error(e)
+        raise DataBaseConnectionError()
+    except Exception as e:
+        app_logger.error(e)
+        raise InternalServerError()
