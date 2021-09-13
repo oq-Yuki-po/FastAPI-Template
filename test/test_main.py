d54709e042842555d6dc8d05a25370e6f2ddf5c5
oq-Yuki-op <o.yukinko0715@gmail.com>
2021-09-13 17:45:49 +0900

    Fix UT

--- test/test_main.py
+++ test/test_main.py
@@ -6,11 +6,18 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 
-def test_main():
+def test_root_get(app_client):
 
-    client = TestClient(app)
+    message = "Testing"
+    response = app_client.get("/", params={"message": message})
 
-    response = client.get("/")
+    assert ast.literal_eval(response.text) == {"message": message}
+    assert response.status_code == status.HTTP_200_OK
 
-    assert ast.literal_eval(response.text) == {"message": "Hello World"}
+def test_root_post(app_client):
+
+    message = "Testing"
+    response = app_client.post("/", json={"message": message})
+
+    assert ast.literal_eval(response.text) == {"message": message}
     assert response.status_code == status.HTTP_200_OK
