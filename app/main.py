0d3dd85c85d36b1adc1e074f74835f801c0562ef
oq-Yuki-po <o.yukinko0715@gmail.com>
2023-04-29 15:52:37 +0900

    test

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
+import yaml
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 from app.middleware.exception_middleware import ExceptionMiddleware
 from app.models.setting import initialize_db, initialize_table
 from app.routers import book_router
 
 app = FastAPI(title="FastAPI Template", version="1.0.0")
-
 
 app.include_router(book_router)
 app.add_middleware(ExceptionMiddleware)
@@ -16,5 +17,22 @@ async def startup_event():
     initialize_db()
     initialize_table()
 
-def print_spec() -> None:
-    print(app.openapi())
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="FastAPI Template",
+        version="1.0.0",
+        description="This is a smple OpenAPI schema",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+def export_swagger():
+
+    print(yaml.dump(custom_openapi()))
+
+if __name__ == "__main__":
+    export_swagger()
