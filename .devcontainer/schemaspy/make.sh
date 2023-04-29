ab9147db292a6db0aedb754fdf3b5e34c3e34697
oq-Yuki-po <o.yukinko0715@gmail.com>
2023-04-29 19:49:46 +0900

    test

--- .devcontainer/schemaspy/make.sh
+++ .devcontainer/schemaspy/make.sh
@@ -1,17 +1,9 @@
-POSTGRES_USER=postgres
-POSTGRES_PASSWORD=postgres
-POSTGRES_SERVER=localhost
-POSTGRES_PORT=5432
-POSTGRES_DB=postgres
-env
-ls -l
-
 /usr/local/bin/schemaspy \
--u ${POSTGRES_USER} \
--host ${POSTGRES_SERVER} \
--port ${POSTGRES_PORT} \
--db ${POSTGRES_DB} \
--p ${POSTGRES_PASSWORD} \
+-u postgres \
+-host localhost \
+-port 5432 \
+-db postgres \
+-p postgres \
 -s public \
 -t pgsql11 \
 -connprops useSSL\\\\=false
